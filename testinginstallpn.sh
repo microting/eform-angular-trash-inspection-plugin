8a5400a93eb7cb137158c3f76b3406c178087f82
Mathias Husum Nielsen <mathiashusumn@gmail.com>
2019-01-28 09:47:35 +0100

    changing paths in script

--- testinginstallpn.sh
+++ testinginstallpn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sed '/\/\/ INSERT ROUTES HERE/i {' src/app/plugins/plugins.routing.ts -i
-sed '/\/\/ INSERT ROUTES HERE/i path: "trashinspection-pn",' src/app/plugins/plugins.routing.ts -i
+sed '/\/\/ INSERT ROUTES HERE/i path: "trash-inspection-pn",' src/app/plugins/plugins.routing.ts -i
 sed '/\/\/ INSERT ROUTES HERE/i canActivate: [AuthGuard],' src/app/plugins/plugins.routing.ts -i
-sed '/\/\/ INSERT ROUTES HERE/i loadChildren: "./modules/trashinspection-pn/trashinspection-pn.module#TrashInspectionPnModule"' src/app/plugins/plugins.routing.ts -i
+sed '/\/\/ INSERT ROUTES HERE/i loadChildren: "./modules/trash-inspection-pn/trash-inspection-pn.module#TrashInspectionPnModule"' src/app/plugins/plugins.routing.ts -i
 sed '/\/\/ INSERT ROUTES HERE/i }' src/app/plugins/plugins.routing.ts -i
 
