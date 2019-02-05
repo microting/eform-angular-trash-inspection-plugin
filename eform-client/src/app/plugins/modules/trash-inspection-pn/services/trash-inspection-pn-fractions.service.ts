490e97753371b7662e02df1bf61442ec60ff1e79
René Schultz Madsen <rm@microting.dk>
2019-02-05 11:47:56 +0100

    Removing the FractionPnCreateModel import.

--- eform-client/src/app/plugins/modules/trash-inspection-pn/services/trash-inspection-pn-fractions.service.ts
+++ eform-client/src/app/plugins/modules/trash-inspection-pn/services/trash-inspection-pn-fractions.service.ts
@@ -7,8 +7,7 @@ import {Router} from '@angular/router';
 import {OperationDataResult, OperationResult} from 'src/app/common/models/operation.models';
 import {BaseService} from 'src/app/common/services/base.service';
 
-import {FractionPnCreateModel,
-        FractionPnModel,
+import {FractionPnModel,
         FractionPnRequestModel,
         FractionPnUpdateModel,
         FractionsPnModel} from '../models/fraction';
