be3140c6d510f3c0b07d629d0f713835eaddf1f0
Mathias Husum Nielsen <mathiashusumn@gmail.com>
2019-02-11 08:49:00 +0100

    adding pluginPage import statement.

--- eform-client/e2e/Tests/trash-inspections-settings/application-settings.plugins-page.spec.ts
+++ eform-client/e2e/Tests/trash-inspections-settings/application-settings.plugins-page.spec.ts
@@ -1,8 +1,9 @@
 import loginPage from '../../Page objects/Login.page';
 import myEformsPage from '../../Page objects/MyEforms.page';
+import pluginsPage from './application-settings.plugins.page';
 
 import {expect} from 'chai';
-import pluginsPage from './application-settings.plugins.page';
+import pluginPage from '../../Page objects/Plugin.page';
 
 describe('Application settings page - site header section', function () {
     before(function () {
