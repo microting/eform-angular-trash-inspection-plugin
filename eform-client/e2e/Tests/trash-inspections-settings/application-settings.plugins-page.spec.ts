b8c5e4ff4b9cc713d97f00f4ea00e09946d03bfd
Mathias Husum Nielsen <mathiashusumn@gmail.com>
2019-02-12 13:20:54 +0100

    name fixing.

--- eform-client/e2e/Tests/trash-inspections-settings/application-settings.plugins-page.spec.ts
+++ eform-client/e2e/Tests/trash-inspections-settings/application-settings.plugins-page.spec.ts
@@ -1,9 +1,9 @@
 import loginPage from '../../Page objects/Login.page';
 import myEformsPage from '../../Page objects/MyEforms.page';
-import pluginsPage from './application-settings.plugins.page';
+import pluginPage from '../../Page objects/Plugin.page';
 
 import {expect} from 'chai';
-import pluginPage from '../../Page objects/Plugin.page';
+import pluginsPage from './application-settings.plugins.page';
 
 describe('Application settings page - site header section', function () {
     before(function () {
