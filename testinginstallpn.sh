5f78c900c57b447a06e5b90fdf8ea2b18cbbc8b7
René Schultz Madsen <rm@microting.dk>
2019-02-20 10:38:27 +0100

    Trying with a different file.

--- testinginstallpn.sh
+++ testinginstallpn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sed '/\/\/ INSERT ROUTES HERE/i {' src/app/plugins/plugins.routing.ts -i
-sed '/\/\/ INSERT ROUTES HERE/i path: "trash-inspection-pn",' src/app/plugins/plugins.routing.ts -i
-sed '/\/\/ INSERT ROUTES HERE/i canActivate: [AuthGuard],' src/app/plugins/plugins.routing.ts -i
-sed '/\/\/ INSERT ROUTES HERE/i loadChildren: "./modules/trash-inspection-pn/trash-inspection-pn.module#TrashInspectionPnModule"' src/app/plugins/plugins.routing.ts -i
-sed '/\/\/ INSERT ROUTES HERE/i }' src/app/plugins/plugins.routing.ts -i
+sed '/\/\/ INSERT ROUTES HERE/i {' /home/travis/build/microting/eform-angular-frontend/eform-client/src/app/plugins/plugins.routing.ts -i
+sed '/\/\/ INSERT ROUTES HERE/i path: "trash-inspection-pn",' /home/travis/build/microting/eform-angular-frontend/eform-client/src/app/plugins/plugins.routing.ts -i
+sed '/\/\/ INSERT ROUTES HERE/i canActivate: [AuthGuard],' /home/travis/build/microting/eform-angular-frontend/eform-client/src/app/plugins/plugins.routing.ts -i
+sed '/\/\/ INSERT ROUTES HERE/i loadChildren: "./modules/trash-inspection-pn/trash-inspection-pn.module#TrashInspectionPnModule"' /home/travis/build/microting/eform-angular-frontend/eform-client/src/app/plugins/plugins.routing.ts -i
+sed '/\/\/ INSERT ROUTES HERE/i }' /home/travis/build/microting/eform-angular-frontend/eform-client/src/app/plugins/plugins.routing.ts -i
 
