25c9a7e703ca3520bdda60f024436e0b8f6f260b
René Schultz Madsen <rm@microting.dk>
2019-03-05 12:59:18 +0100

    Adding initial install.sh file.

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+dotnet restore eFormAPI/Plugins/TrashInspection.Pn/TrashInspection.Pn.sln
+dotnet build eFormAPI/Plugins/TrashInspection.Pn/TrashInspection.Pn.sln --runtime linux-x64 --configuration Release
+cd ../..
+cp -av microting/eform-angular-trashinspection-plugin/eform-client/src/app/plugins/modules/trash-inspection-pn microting/eform-angular-frontend/eform-client/src/app/plugins/modules/trash-inspection-pn
+mkdir -p microting/eform-angular-frontend/eFormAPI/eFormAPI.Web/Plugins
+cp -av microting/eform-angular-trashinspection-plugin/eFormAPI/eFormAPI.Web/Plugins/TrashInspection microting/eform-angular-frontend/eFormAPI/eFormAPI.Web/out/Plugins/TrashInspection
+
+
+echo "Recompile angular"
+cd microting/eform-angular-frontend/eform-client
+../../microting/eform-angular-trashinspection-plugin/testinginstallpn.sh
+su ubuntu -c \
+"npm run build"
+echo "Recompiling angular done"
+
+export plugin_db_name="EFormTrashInspectionPn"
