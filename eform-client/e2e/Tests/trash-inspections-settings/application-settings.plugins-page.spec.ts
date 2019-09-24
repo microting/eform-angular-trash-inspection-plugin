1423b0351f6d34f31162cd251bd2197f14044ebb
René Schultz Madsen <rm@microting.dk>
2019-09-24 08:10:46 +0200

    Doing a bit of cleanup.

--- eform-client/e2e/Tests/trash-inspections-settings/application-settings.plugins-page.spec.ts
+++ eform-client/e2e/Tests/trash-inspections-settings/application-settings.plugins-page.spec.ts
@@ -8,49 +8,35 @@ import pluginsPage from './application-settings.plugins.page';
 describe('Application settings page - site header section', function () {
     before(function () {
         loginPage.open('/auth');
-        loginPage.login();
     });
     it('should go to plugin settings page', function () {
+        loginPage.login();
         myEformsPage.Navbar.advancedDropdown();
         myEformsPage.Navbar.clickonSubMenuItem('Plugins');
-        // browser.pause(8000);
-        browser.waitForVisible('#plugin-id', 80000);
-        browser.pause(20000);
+        browser.pause(40000);
 
         const plugin = pluginsPage.getFirstPluginRowObj();
         expect(plugin.id).equal(1);
         expect(plugin.name).equal('Microting Trash Inspection Plugin');
         expect(plugin.version).equal('1.0.0.0');
         expect(plugin.status).equal('Deaktiveret');
-        // expect()
 
     });
 
     it('should activate the plugin', function () {
         pluginPage.pluginSettingsBtn.click();
-        // browser.pause(8000);
-        browser.waitForVisible('#PluginDropDown', 80000);
-        browser.pause(20000);
+        browser.waitForVisible('#PluginDropDown', 40000);
         pluginPage.selectValue('PluginDropDown', 'PluginDropDown', 'Aktiveret');
-        // browser.pause(8000);
         pluginPage.saveBtn.click();
         browser.pause(2000);
         browser.refresh();
 
-        browser.waitForVisible('#plugin-id', 80000);
-        browser.pause(10000);
+        browser.pause(20000);
         const plugin = pluginsPage.getFirstPluginRowObj();
         expect(plugin.id).equal(1);
         expect(plugin.name).equal('Microting Trash Inspection Plugin');
         expect(plugin.version).equal('1.0.0.0');
         expect(plugin.status).equal('Aktiveret');
         expect(browser.element(`//*[contains(@class, 'dropdown')]//*[contains(text(), 'Affaldsinspektion')]`).isExisting()).equal(true);
-        // click on plugin settings
-        // enter connectionstring for customers plugin
-        // select activate
-        // save changes
-        // see that the plugin is marked active
-        // validate that the customers menu entry is now visible
-        // validate that the customers index page is shown with all fields active in the header
     });
 });
