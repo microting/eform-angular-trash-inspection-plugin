fcd5fae701fd2a382fcaf9bb1f69fcf4547671e8
René Schultz Madsen <rm@microting.com>
2019-10-09 17:47:40 +0200

    Adding changes to copy files back to repository from development.

--- devgetchanges.sh
+++ devgetchanges.sh
@@ -2,13 +2,13 @@
 
 cd ~
 
-if [ -d "Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/src/app/plugins/modules/trash-inspection-pn"]; then
+if [ -d "Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/src/app/plugins/modules/trash-inspection-pn" ]; then
 	rm -fR Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/src/app/plugins/modules/trash-inspection-pn
 fi
 
 cp -av Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/trash-inspection-pn Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/src/app/plugins/modules/trash-inspection-pn
 
-if [ -d "Documents/workspace/microting/eform-angular-trashinspection-plugin/eFormAPI/Plugins/TrashInspection.Pn"]; then
+if [ -d "Documents/workspace/microting/eform-angular-trashinspection-plugin/eFormAPI/Plugins/TrashInspection.Pn" ]; then
 	rm -fR Documents/workspace/microting/eform-angular-trashinspection-plugin/eFormAPI/Plugins/TrashInspection.Pn
 fi
 
--- devinstall.sh
+++ devinstall.sh
@@ -4,14 +4,12 @@ pwd
 
 if [ -d "Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/trash-inspection-pn" ]; then
 	rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/trash-inspection-pn
-	echo "folder exists"
 fi
 
 cp -av Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/src/app/plugins/modules/trash-inspection-pn Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/trash-inspection-pn
 
 if [ -d "Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/TrashInspection.Pn" ]; then
 	rm -fR Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/TrashInspection.Pn
-	echo "folder plugins exists"
 fi
 
 cp -av Documents/workspace/microting/eform-angular-trashinspection-plugin/eFormAPI/Plugins/TrashInspection.Pn Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/TrashInspection.Pn
