461c72efad233a0c5ae4e8dad6f9841ae0fa5404
René Schultz Madsen <rm@microting.com>
2019-12-18 06:51:34 +0100

    Fixing the testinsinstallpn.sh

--- testinginstallpn.sh
+++ testinginstallpn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed '/\/\/ INSERT ROUTES HERE/i ,{' src/app/plugins/plugins.routing.ts -i
+sed '/\/\/ INSERT ROUTES HERE/i {' src/app/plugins/plugins.routing.ts -i
 sed '/\/\/ INSERT ROUTES HERE/i path: "trash-inspection-pn",' src/app/plugins/plugins.routing.ts -i
 sed '/\/\/ INSERT ROUTES HERE/i canActivate: [AuthGuard],' src/app/plugins/plugins.routing.ts -i
 sed '/\/\/ INSERT ROUTES HERE/i loadChildren: "./modules/trash-inspection-pn/trash-inspection-pn.module#TrashInspectionPnModule"' src/app/plugins/plugins.routing.ts -i
