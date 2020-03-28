ab0f39ee3beeba4e8a0220ae6976d2270a8906ee
René Schultz Madsen <rm@microting.dk>
2020-03-28 09:47:34 +0100

    Fixing the broken copy.

--- devinstall.sh
+++ devinstall.sh
@@ -11,7 +11,6 @@ rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Pag
 rm -fR Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.js
 
 cp -a Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/src/app/plugins/modules/trash-inspection-pn Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/trash-inspection-pn
-cp -a Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/src/app/plugins/modules/trash-inspection-pn Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/trash-inspection-pn
 cp -a Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/e2e/Tests/trash-inspections-settings Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/trash-inspections-settings
 cp -a Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/e2e/Tests/trash-inspection-general Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/trash-inspection-general
 cp -a Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/e2e/Page\ objects/trash-inspection Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/trash-inspection
