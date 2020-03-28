5263ec0b2dbdae1fb0bdce4004cd4dd0d7c50562
René Schultz Madsen <rm@microting.dk>
2020-03-28 09:34:55 +0100

    Fixing the Page object name.

--- devgetchanges.sh
+++ devgetchanges.sh
@@ -14,12 +14,12 @@ cp -r Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/Tras
 
 rm -fR Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/e2e/Tests/trash-inspections-settings
 rm -fR Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/e2e/Tests/trash-inspection-general 
-rm -fR Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/e2e/Page\ objects/trashinspection
+rm -fR Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/e2e/Page\ objects/trash-inspection
 rm -fR Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/wdio-headless-plugin-step2.conf.js
 
 # Test files cp
 
 cp -r Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/trash-inspections-settings Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/e2e/Tests/trash-inspections-settings
 cp -r Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/trash-inspection-general Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/e2e/Tests/trash-inspection-general 
-cp -r Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/trashinspection Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/e2e/Page\ objects/trashinspection
+cp -r Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/trash-inspection Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/e2e/Page\ objects/trash-inspection
 cp -r Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.js Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/wdio-headless-plugin-step2.conf.js
