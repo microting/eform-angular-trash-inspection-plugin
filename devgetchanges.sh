aadba42690c6e5a621268e0c38c0b17f8ae77407
René Schultz Madsen <rm@microting.dk>
2020-03-28 09:39:56 +0100

    Updating the devgetchanges.

--- devgetchanges.sh
+++ devgetchanges.sh
@@ -4,11 +4,11 @@ cd ~
 
 rm -fR Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/src/app/plugins/modules/trash-inspection-pn
 
-cp -r Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/trash-inspection-pn Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/src/app/plugins/modules/trash-inspection-pn
+cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/src/app/plugins/modules/trash-inspection-pn Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/src/app/plugins/modules/trash-inspection-pn
 
 rm -fR Documents/workspace/microting/eform-angular-trashinspection-plugin/eFormAPI/Plugins/TrashInspection.Pn
 
-cp -r Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/TrashInspection.Pn Documents/workspace/microting/eform-angular-trashinspection-plugin/eFormAPI/Plugins/TrashInspection.Pn
+cp -a Documents/workspace/microting/eform-angular-frontend/eFormAPI/Plugins/TrashInspection.Pn Documents/workspace/microting/eform-angular-trashinspection-plugin/eFormAPI/Plugins/TrashInspection.Pn
 
 # Test files rm
 
@@ -19,7 +19,7 @@ rm -fR Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-
 
 # Test files cp
 
-cp -r Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/trash-inspections-settings Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/e2e/Tests/trash-inspections-settings
-cp -r Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/trash-inspection-general Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/e2e/Tests/trash-inspection-general 
-cp -r Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/trash-inspection Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/e2e/Page\ objects/trash-inspection
-cp -r Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.js Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/wdio-headless-plugin-step2.conf.js
+cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/trash-inspections-settings Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/e2e/Tests/trash-inspections-settings
+cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Tests/trash-inspection-general Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/e2e/Tests/trash-inspection-general 
+cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/e2e/Page\ objects/trash-inspection Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/e2e/Page\ objects/trash-inspection
+cp -a Documents/workspace/microting/eform-angular-frontend/eform-client/wdio-plugin-step2.conf.js Documents/workspace/microting/eform-angular-trashinspection-plugin/eform-client/wdio-headless-plugin-step2.conf.js
