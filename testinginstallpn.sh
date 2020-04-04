7fca3b97082e7921dc792bdd2b514fc58cb272ec
René Schultz Madsen <rm@microting.dk>
2020-04-04 11:13:34 +0200

    Fixing small bugs in the testinginstall.pn and travis file.

--- testinginstallpn.sh
+++ testinginstallpn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-perl -pi -e '$_.="  }\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
+perl -pi -e '$_.="  },\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
 perl -pi -e '$_.="  loadChildren: '\''./modules/trash-inspection-pn/trash-inspection-pn.module#TrashInspectionPnModule'\''\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
 perl -pi -e '$_.="  canActivate: [AuthGuard],\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
 perl -pi -e '$_.="  path: '\''trash-inspection-pn'\'',\n" if /INSERT ROUTES HERE/' src/app/plugins/plugins.routing.ts
