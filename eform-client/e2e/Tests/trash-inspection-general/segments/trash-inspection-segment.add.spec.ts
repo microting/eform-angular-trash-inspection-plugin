cc33d8b00a447e54c3379b49f1b4ea4353c52bdd
Alex Horban <sahok6264@gmail.com>
2023-06-20 21:09:59 +0300

    Refactor TrashInspectionSegmentsPage class methods (tests)
    This commit refactors the TrashInspectionSegmentsPage class methods to improve readability and maintainability. The changes include renaming a misspelled method, removing unnecessary code, adding optional parameters to some methods, and splitting a long method into smaller ones.

--- eform-client/e2e/Tests/trash-inspection-general/segments/trash-inspection-segment.add.spec.ts
+++ eform-client/e2e/Tests/trash-inspection-general/segments/trash-inspection-segment.add.spec.ts
@@ -1,56 +1,57 @@
 import {expect} from 'chai';
 import loginPage from '../../../Page objects/Login.page';
-import segmentPage from '../../../Page objects/trash-inspection/TrashInspection-Segment.page';
-import {Guid} from 'guid-typescript';
+import segmentPage, {CreateUpdateSegment} from '../../../Page objects/trash-inspection/TrashInspection-Segment.page';
+import {generateRandmString, getRandomInt} from '../../../Helpers/helper-functions';
+
+const createModel: CreateUpdateSegment = {
+  name: generateRandmString(),
+  description: generateRandmString(),
+  sdkFolderId: getRandomInt(1, 10).toString(),
+};
 
 describe('Trash Inspection Plugin - Segment', function () {
   before(async () => {
     await loginPage.open('/auth');
     await loginPage.login();
+    await segmentPage.goToSegmentsPage();
   });
   it('Should create segment.', async () => {
-    const name = Guid.create().toString();
-    const description = Guid.create().toString();
-    const sdkFolderId = Math.floor((Math.random() * 10) + 1);
-    await segmentPage.goToSegmentsPage();
-    await segmentPage.createSegment(name, description, sdkFolderId);
+    await segmentPage.createSegment(createModel);
     const segment = await segmentPage.getFirstRowObject();
-    expect(segment.name).equal(name);
-    expect(segment.description).equal(description);
-    expect(segment.sdkFolderId).equal(`${sdkFolderId}`);
-    await segmentPage.deleteSegment();
+    expect(segment.name).equal(createModel.name);
+    expect(segment.description).equal(createModel.description);
+    expect(segment.sdkFolderId).equal(createModel.sdkFolderId);
   });
   it('Should create segment with only Name.', async () => {
-    const name = Guid.create().toString();
-    await segmentPage.createSegment(name);
+    const name = generateRandmString();
+    await segmentPage.createSegment({name: name});
     const segment = await segmentPage.getFirstRowObject();
     expect(segment.name).equal(name);
-    await segmentPage.deleteSegment();
   });
   it('Should create segment with name and description.', async () => {
-    const name = Guid.create().toString();
-    const description = Guid.create().toString();
-    await segmentPage.createSegment(name, description);
+    const name = generateRandmString();
+    const description = generateRandmString();
+    await segmentPage.createSegment({name: name, description: description});
     const segment = await segmentPage.getFirstRowObject();
     expect(segment.name).equal(name);
     expect(segment.description).equal(description);
-    await segmentPage.deleteSegment();
   });
   it('Should create segment with name and sdkFolderId.', async () => {
-    const name = Guid.create().toString();
-    const sdkFolderId = Math.floor((Math.random() * 10) + 1);
-    await segmentPage.createSegment(name, '', sdkFolderId);
+    const name = generateRandmString();
+    const sdkFolderId = getRandomInt(1, 10).toString();
+    await segmentPage.createSegment({name: name, sdkFolderId: sdkFolderId});
     const segment = await segmentPage.getFirstRowObject();
     expect(segment.name).equal(name);
-    expect(segment.sdkFolderId).equal(`${sdkFolderId}`);
-    await segmentPage.deleteSegment();
+    expect(segment.sdkFolderId).equal(sdkFolderId);
   });
   it('should not create segment', async () => {
-    const name = Guid.create().toString();
-    const description = Guid.create().toString();
-    const sdkFolderId = Math.floor((Math.random() * 10) + 1);
-    await segmentPage.goToSegmentsPage();
-    await segmentPage.createSegment_cancel(name, description, sdkFolderId);
+    const name = generateRandmString();
+    const description = generateRandmString();
+    const sdkFolderId = getRandomInt(1, 10).toString();
+    await segmentPage.createSegment({name: name, description: description, sdkFolderId: sdkFolderId}, true);
     expect(await segmentPage.rowNum()).equal(0);
+  });
+  afterEach(async () => {
+    await segmentPage.clearTable();
   });
 });
--- eform-client/wdio-headless-plugin-step2.conf.ts
+++ eform-client/wdio-headless-plugin-step2.conf.ts
@@ -23,9 +23,9 @@ export const config: Options.Testrunner = {
         'e2e/Tests/trash-inspection-general/fractions/trash-inspection-fraction.add.spec.ts',
         'e2e/Tests/trash-inspection-general/fractions/trash-inspection-fraction.edit.spec.ts',
         'e2e/Tests/trash-inspection-general/fractions/trash-inspection-fraction.delete.spec.ts',
-        //'e2e/Tests/trash-inspection-general/segments/trash-inspection-segment.add.spec.ts', // TODO Fix it
-        //'e2e/Tests/trash-inspection-general/segments/trash-inspection-segment.edit.spec.ts', // TODO Fix it
-        //'e2e/Tests/trash-inspection-general/segments/trash-inspection-segment.delete.spec.ts', // TODO Fix it
+        'e2e/Tests/trash-inspection-general/segments/trash-inspection-segment.add.spec.ts',
+        'e2e/Tests/trash-inspection-general/segments/trash-inspection-segment.edit.spec.ts',
+        'e2e/Tests/trash-inspection-general/segments/trash-inspection-segment.delete.spec.ts',
     ],
     suites: {
         settings: [
